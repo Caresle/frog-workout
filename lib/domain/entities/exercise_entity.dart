42605585b55edf57e2956a812ae9223ed999a960
Carlos Estrada <carabesle@gmail.com>
2025-07-24 17:31:47 -0600

    feat(entities): add exercise

---
+++ lib/domain/entities/exercise_entity.dart
@@ -0,0 +1,33 @@
+enum WeightType { kg, lbs }
+
+class Exercise {
+  final int id;
+  final String name;
+  final WeightType weightType;
+  final bool customCreated;
+  final int? idUser;
+
+  const Exercise({
+    required this.id,
+    required this.name,
+    required this.weightType,
+    this.customCreated = true,
+    this.idUser,
+  });
+
+  Exercise copyWith({
+    int? id,
+    String? name,
+    WeightType? weightType,
+    bool? customCreated,
+    int? idUser,
+  }) {
+    return Exercise(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      weightType: weightType ?? this.weightType,
+      customCreated: customCreated ?? this.customCreated,
+      idUser: idUser ?? this.idUser,
+    );
+  }
+}
