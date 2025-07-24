02b5442c196c4888e0a19b9c5dcfccc6751bd5d9
Carlos Estrada <carabesle@gmail.com>
2025-07-24 12:06:11 -0600

    feat(widget): add actions in header exercise

--- lib/widgets/workout_item/exercise_detail_header.dart
+++ lib/widgets/workout_item/exercise_detail_header.dart
@@ -5,6 +5,8 @@ class ExerciseDetailHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -15,7 +17,59 @@ class ExerciseDetailHeader extends StatelessWidget {
             Text('Exercise'),
           ],
         ),
-        IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz_rounded)),
+        IconButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: deviceSize.width,
+                    height: deviceSize.height * 0.4,
+                    child: Column(
+                      children: [
+                        const Text('Actions'),
+                        const SizedBox(height: 16),
+                        FilledButton.tonal(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Icon(Icons.swap_vert_rounded),
+                              const SizedBox(width: 8),
+                              Text('Reorder'),
+                            ],
+                          ),
+                        ),
+                        FilledButton.tonal(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Icon(Icons.edit_rounded),
+                              const SizedBox(width: 8),
+                              Text('Replace Exercise'),
+                            ],
+                          ),
+                        ),
+                        FilledButton.tonal(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Icon(Icons.close_rounded),
+                              const SizedBox(width: 8),
+                              Text('Remove Exercise'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+          icon: Icon(Icons.more_horiz_rounded),
+        ),
       ],
     );
   }
