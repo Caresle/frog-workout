e7013c0b9365f230182e822bab18c284a8692743
Carlos Estrada <carabesle@gmail.com>
2025-07-28 11:17:27 -0600

    feat: add loading of workouts on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:workouts_app/providers/providers.dart';
 import 'package:workouts_app/widgets/widgets.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isInit = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (_isInit) return;
+
+    _isInit = true;
+    Future.microtask(() {
+      if (!mounted) return;
+
+      final provider = Provider.of<WorkoutsProvider>(context, listen: false);
+      provider.getAll();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +39,8 @@ class HomeScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               NewWorkoutButton(),
-              const Text("Workouts"),
-              const SizedBox(height: 16),
+              // const Text("Workouts"),
+              // const SizedBox(height: 16),
               WorkoutsList(),
             ],
           ),
