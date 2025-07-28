7463c338bb7b5a67c7f34927cfb46fa9f2e94638
Carlos Estrada <carabesle@gmail.com>
2025-07-28 11:17:00 -0600

    feat: add redirect to workout_detail screen on new_workout

--- lib/widgets/workouts/new_workout_button.dart
+++ lib/widgets/workouts/new_workout_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class NewWorkoutButton extends StatelessWidget {
   const NewWorkoutButton({super.key});
@@ -6,7 +7,9 @@ class NewWorkoutButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FilledButton.tonal(
-      onPressed: () {},
+      onPressed: () {
+        context.push('/workouts/-1');
+      },
       child: Row(
         children: [
           Icon(Icons.add_rounded),
