18591a3b85839a0dd52461d8c26b9cd4486f367f
Carlos Estrada <carabesle@gmail.com>
2025-08-01 16:30:32 -0600

    feat(ui): add screens for specific data visualization

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -6,3 +6,8 @@ export './workout_display_screen.dart';
 export './exercise_list_screen.dart';
 export './settings_screen.dart';
 export './progress_screen.dart';
+
+// progress
+export './progress/statistics_screen.dart';
+export './progress/measures_screen.dart';
+export './progress/calendar_screen.dart';
