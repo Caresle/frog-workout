96765b7c563f55fa7ca5c5d2995b75df158ebf5d
Carlos Estrada <carabesle@gmail.com>
2025-08-06 11:49:42 -0600

    feat(auth): add auth provider to multiprovider

---
+++ lib/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,45 @@
+import 'package:workouts_app/data/datasources/auth/auth_handler_ds.dart';
+import 'package:workouts_app/domain/domain.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  final AuthHandlerDs _localDs;
+  final AuthHandlerDs _remoteDs;
+
+  AuthRepositoryImpl({
+    required AuthHandlerDs local,
+    required AuthHandlerDs remote,
+  }) : _localDs = local,
+       _remoteDs = remote;
+
+  @override
+  Future<bool> isAuthenticated() async {
+    final isLocal = await _localDs.isAuthenticated();
+    if (isLocal) return true;
+
+    final isRemote = await _remoteDs.isAuthenticated();
+
+    return isRemote;
+  }
+
+  @override
+  Future<bool> isLocalAuthenticated() {
+    return _localDs.isAuthenticated();
+  }
+
+  @override
+  Future<bool> isRemoteAuthenticated() {
+    return _remoteDs.isAuthenticated();
+  }
+
+  @override
+  Future<bool> signIn() {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:workouts_app/domain/domain.dart';
+
+class AuthProvider extends ChangeNotifier {
+  final AuthRepository _repository;
+
+  AuthProvider(this._repository);
+}
